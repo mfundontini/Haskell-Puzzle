01fee75e75dcbc9eee30e8da01e3d7b01cc6366b
MfundoNtini <mfundontini@gmail.com>
2020-04-05 21:44:19 +0200

    Started testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,4 +3,11 @@ import Lib
 
 -- do describe do it do function should be assert
 main :: IO ()
-main = hspec $ do describe "How to write a test" $ do it "Should return a string Hello world" $ do myFunction `shouldBe` "Hello World"
+main = hspec $ do
+    describe "How to write a test using the myFuction test function" $ do
+        it "Should return a string Hello world" $ do
+            myFunction `shouldBe` "Hello World"
+
+    describe "The function format grid is supposed to append newlines to a list of strings and return a string" $ do
+        it "Should return\n SARS\n MERS\n SAIL\n BAIL" $ do
+            (formatGrid ["SARS", "MERS", "SAIL", "BAIL"]) `shouldBe` "SARS\n MERS\n SAIL\n BAIL\n"
