f90bd5e3cca644dd7f701347e4430e25d5a866a0
MfundoNtini <mfundontini@gmail.com>
2020-04-06 03:56:58 +0200

    Added deep screen creation and rendering code

--- src/Game.hs
+++ src/Game.hs
@@ -1,4 +1,11 @@
-module Game (description) where
+-- Exports
+module Game
+        ( description,
+          outputInteractiveGrid,
+          createArbitrayGrid,
+          createDeepScreen,
+          createLimitedDeepScreen
+        ) where
 
 -- Nested iteration [[(column, row) | column <- [0..7]]| row <- [0..7]] read as for each row, do this with the columns
 -- Or  map (\s -> zip ([0..7]) s) (map (take 10 . repeat) [i | i <- [0..7]]) which is more complicated
@@ -14,5 +21,36 @@ module Game (description) where
 --	[(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7)]
 --]
 
+import Const (grid)
+
+-- An alias for a list of lists of tuples containing (column, row)
+type Screen = [[(Int, Int)]]
+-- A screen superimposed with a character for a 3D effect
+type DeepScreen = [[((Int, Int), Char)]]
+
+-- Make a string description for this module
 description :: String
 description = "This is an interactive version of the puzzle on Lib.hs"
+
+-- Output the screen in a pretty manner
+outputInteractiveGrid :: Show a => [a] -> IO ()
+outputInteractiveGrid = putStrLn . unlines . map show
+
+-- Create a screen of arbitrary X by X side
+createArbitrayGrid :: Int -> Screen
+createArbitrayGrid 0 = [[(0,0)]]
+createArbitrayGrid side = map (\s -> zip ([0..side - 1]) s) (map (take side . repeat) [i | i <- [0..side - 1]])
+
+-- Create a deep screen bounded by a square screen size
+createLimitedDeepScreen :: Int -> DeepScreen
+createLimitedDeepScreen side = let
+                rows = map (take side . repeat) [0..]
+                columns = (take side . repeat) [0..] in
+                    zipWith zip (zipWith zip columns rows) (grid)
+
+-- Create a deep screen only bound by character puzzle
+createDeepScreen :: DeepScreen
+createDeepScreen = let
+                rows = map (repeat) [0..]
+                columns = (repeat) [0..] in
+                    zipWith zip (zipWith zip columns rows) (grid)
